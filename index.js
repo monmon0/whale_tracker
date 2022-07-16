5bd86aeff6062aaa672a0729651a8e8284b6e9fd
monmon <107778876+monmon0@users.noreply.github.com>
2022-07-16 15:42:02 -0600

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your whale tracker script here!
+const ethers = require('ethers')
+
+const rpcUrl = 'https://cloudfare-eth.com/'
+const provider = new ethers.providers.JsonRpcBatchProvider(rpcUrl)
+
+const main = async () =>{
+    
+}
+
+main()
